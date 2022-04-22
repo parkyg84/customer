ca1c67cb2afea452fab033a40f5c9946d8bfcdcb
parky <parkyg84@naver.com>
2022-04-22 15:32:00 +0900

    회원정보호출 셋팅

--- src/main/java/com/example/customer/CustomerApplication.java
+++ src/main/java/com/example/customer/CustomerApplication.java
@@ -2,12 +2,20 @@ package com.example.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
-public class CustomerApplication {
+public class CustomerApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(CustomerApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(CustomerApplication.class);
+        //
+    }
 }
--- src/main/java/com/example/customer/controller/CustomerController.java
+++ src/main/java/com/example/customer/controller/CustomerController.java
@@ -1,2 +1,21 @@
-package com.example.customer.controller;public class CustomerController {
+package com.example.customer.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("CustomerApi")
+public class CustomerController {
+
+    //@ResponseBody
+    @RequestMapping(method = RequestMethod.GET, path ="/getCustInfo")
+    public String CustomerHome()
+    {
+        System.out.println("customerId start");
+
+        return "customerId is yongpark";
+    }
 }
